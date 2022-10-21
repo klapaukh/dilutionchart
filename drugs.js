a75a8b3bd5d76ec511534a547ad1ac30197892c1
Roma Klapaukh <rklapaukh@hotmail.com>
2022-10-21 15:17:23 +1100

    Sample drug data

---
+++ drugs.js
@@ -0,0 +1,28 @@
+const full_data = [
+    {
+        "Drug": "Adrenaline",
+        "Audience": "Adult",
+        "Preparation": "3mg made to 50ml with 0.9% NaCl",
+        "Concentration": "60mcg/ml",
+        "Infusion Rate": "5-50mcg/min (5-50ml/hr)",
+        "Total Dose / Volume": "PRN",
+        "Bolus": [
+            "30mcg (0.5ml)",
+            "60mcg (0.5ml)",
+            "90mcg (1.5ml)"
+        ],
+        "Comments": "Start at 5mcg/min (5ml/hr) increasing by 5mcg/min (5ml/hr) increments every 2 mins titrating to effect. Reassess delivery system if dose rate beyond 50mcg/min is required."
+    },
+    {
+        "Drug": "Adrenaline",
+        "Audience": "Paed",
+        "Preparation": "3mg made to 50ml with 0.9% NaCl",
+        "Concentration": "60mcg/ml",
+        "Infusion Rate": "0.1-1mcg/min (0.1-1ml/hr)",
+        "Total Dose / Volume": "PRN",
+        "Bolus": [
+            "Nil"
+        ],
+        "Comments": "Start at 0.1mcg/kg/min (0.1ml/kg/hr) increasing by 0.1mcg/kg/min (0.1ml/kg/hr) increments every 2 mins titrating to effect."
+    }
+    ]
